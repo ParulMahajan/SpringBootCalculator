0f06db1d7a0dddfb4d39b50e552a401142652c72
AZURE_PARUL\ADT032 <parul@azurethailand.com>
2020-06-10 18:15:05 +0700

    added few print statements

--- src/main/java/com/central/controller/CalculatorController.java
+++ src/main/java/com/central/controller/CalculatorController.java
@@ -40,6 +40,8 @@ public class CalculatorController {
 
 					// using single instance of calculator class.
 					double result =  Calculator.getInstance().makeCalculation(paramd1, paramd2, operandc1);
+					
+					System.out.println("result: "+result);
 					return String.valueOf(result);
 				}
 			}
@@ -60,8 +62,8 @@ public class CalculatorController {
 	@RequestMapping(value="/addition/{param1}/{param2}", method=RequestMethod.GET)
 	public String getAddition(@PathVariable String param1, @PathVariable String param2){
 
-		System.out.println(param1);
-		System.out.println(param2);
+		System.out.println("param1: "+param1);
+		System.out.println("param2: "+param2);
 		try {
 
 			// For very large numbers, we can use BigDecimal and BigInteger.
@@ -83,8 +85,8 @@ public class CalculatorController {
 	@RequestMapping(value="/subtraction/{param1}/{param2}", method=RequestMethod.GET)
 	public String getSubtraction(@PathVariable String param1, @PathVariable String param2){
 
-		System.out.println(param1);
-		System.out.println(param2);
+		System.out.println("param1: "+param1);
+		System.out.println("param2: "+param2);
 		try {
 
 			// For very large numbers, we can use BigDecimal and BigInteger.
@@ -103,8 +105,8 @@ public class CalculatorController {
 	@RequestMapping(value="/multlipication/{param1}/{param2}", method=RequestMethod.GET)
 	public String getMultiplication(@PathVariable String param1, @PathVariable String param2){
 
-		System.out.println(param1);
-		System.out.println(param2);
+		System.out.println("param1: "+param1);
+		System.out.println("param2: "+param2);
 		try {
 
 			// For very large numbers, we can use BigDecimal and BigInteger.
@@ -123,8 +125,8 @@ public class CalculatorController {
 	@RequestMapping(value="/division/{param1}/{param2}", method=RequestMethod.GET)
 	public String getDivision(@PathVariable String param1, @PathVariable String param2){
 
-		System.out.println(param1);
-		System.out.println(param2);
+		System.out.println("param1: "+param1);
+		System.out.println("param2: "+param2);
 		try {
 
 			// For very large numbers, we can use BigDecimal and BigInteger.
